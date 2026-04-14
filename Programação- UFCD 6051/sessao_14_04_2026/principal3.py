231d23e74181007acc781c900b648b8055900163
LuisFaria1991 <luisfaria_10@hotmail.com>
2026-04-14 23:05:19 +0100

    asd

---
+++ Programação- UFCD 6051/sessao_14_04_2026/principal3.py
@@ -0,0 +1,11 @@
+#ciclo principal (or)
+while True:
+    #dados de entrada
+    porta = False
+    janela = False
+
+    # processamento
+    if porta or janela:
+        ligar_alarme = True
+    else:
+        ligar_alarme = False
---
+++ Programação- UFCD 6051/sessao_14_04_2026/principal6.py
@@ -0,0 +1,12 @@
+# ciclo principal
+while True:
+
+    # dados de entrada
+    humidadde_solo1 = False
+    humidadde_solo2 = True
+
+    # processamento
+    if not (humidadde_solo1 and humidadde_solo2):
+        ligar_sistema_rega : True
+    else:
+        ligar_sistema_rega = False
